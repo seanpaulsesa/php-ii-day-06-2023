d1bcf7655a478931bf6742367ccd14cd0ca9d4dd
seanpaulsesa <pauluspaul7700@gmail.com>
2023-04-12 18:57:29 +0900

    menambahkan file index.php

---
+++ include/footer.php
@@ -0,0 +1,4 @@
+
+<footer class="border border-gray-600 container mx-auto p-6">
+        <p>Copyright &copy 2023, SaCode.Developer by <span class="font-semibold">Paulus Sesa</span></p>
+    </footer>
